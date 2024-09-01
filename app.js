972ec646d162ef9c8a41a7ef90b0ce39aa1e2b25
Himanshu Singh <110761890+HimanshuSingh82@users.noreply.github.com>
2024-09-01 16:41:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+import dotenv from "dotenv"
+dotenv.config({
+ path:"./.env"
+})
 import express from "express";
 import { dbConnection } from "./database/dbConnection.js";
 import { config } from "dotenv";
